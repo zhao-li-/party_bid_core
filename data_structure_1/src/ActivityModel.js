6628f3b89cc744c5c5f4938ba54473de275bc86a
zhao-li- <s1216186168@gmail.com>
2014-04-03 14:39:02 +0800

    activity_spec

--- data_structure_1/src/ActivityModel.js
+++ data_structure_1/src/ActivityModel.js
@@ -1,3 +1,13 @@
-function Activity(){
-
+function Activity(activity_name) {
+    this.name = activity_name
+    this.sign_ups = []
+    this.bids = []
+}
+Activity.prototype.create = function () {
+    var activities = JSON.parse(localStorage.activities);
+    activities.unshift(this);
+    localStorage.setItem("activities", JSON.stringify(activities));
+}
+Activity.prototype.active = function () {
+    localStorage.current_activity = this.name;
 }
