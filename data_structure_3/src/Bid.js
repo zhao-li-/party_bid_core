93704edbede71440880705a215097871e8df5eba
zhao-li- <s1216186168@gmail.com>
2014-04-06 11:02:32 +0800

    bid_spec

---
+++ data_structure_3/src/Bid.js
@@ -0,0 +1,20 @@
+function Bid (activity_id){
+    var count = Bid.get_this_bids(activity_id).length +1;
+    this.name = "竞价" + count;
+    this.activity_name = activity_id;
+    this.biddings = [];
+}
+Bid.get_bids = function(){
+    return JSON.parse(localStorage.bids);
+}
+Bid.get_this_bids = function(activity_id){
+    return _.filter(Bid.get_bids(),function(bid){
+        return bid.activity_name == activity_id;
+    })
+}
+Bid.create_new_bid = function(activity_id){
+    var bids = Bid.get_bids();
+    var bid = new Bid(activity_id);
+    bids.push(bid);
+    localStorage.setItem("bids",JSON.stringify(bids));
+}
